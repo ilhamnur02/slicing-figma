2d4e2c12864bac6023919a3fe3758fb3281ed6a8
rasyasultanpasha-ship-it <rasyasultanpasha@gmail.com>
2025-12-31 10:40:10 +0800

    dashboard

--- resources/views/eDocument/Dashboard.blade.php
+++ resources/views/eDocument/Dashboard.blade.php
@@ -1,1 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="welcome">
+    <h3>Welcome To eDocument</h3>
+    <p>Please download User Manual for detail information</p>
+</div>
+
+<div class="cards">
+    <div class="card blue">
+        <h4>0</h4>
+        <p>On Progress</p>
+    </div>
+
+    <div class="card green">
+        <h4>458</h4>
+        <p>Completed</p>
+    </div>
+
+    <div class="card orange">
+        <h4>1</h4>
+        <p>Revised</p>
+    </div>
+
+    <div class="card red">
+        <h4>26</h4>
+        <p>Rejected</p>
+    </div>
+
+    <div class="card cyan">
+        <h4>0</h4>
+        <p>Need Approval</p>
+    </div>
+
+    <div class="card darkgreen">
+        <h4>82</h4>
+        <p>New Received</p>
+    </div>
+</div>
+@endsection
 
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+use App\Http\Controllers\DashboardController;
+
+Route::get('/dashboard', [DashboardController::class, 'index']);
