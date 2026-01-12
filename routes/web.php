3ae679f8d0ef2bae45d9aece9dd315ca2fcc568c
ilhamnur02 <ilhamnurilham175@gmail.com>
2026-01-12 11:05:03 +0800

    Update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Dashboardblade');
+    return view('Dashboard.blade.php');
 });
 
 Route::get('/approval', function () {
